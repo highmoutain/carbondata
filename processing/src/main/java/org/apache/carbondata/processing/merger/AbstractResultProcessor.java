5ad47dcf825086df4c44e1ddae0b6d4084ba5982
changchun wang <changchun.wang@tendcloud.com>
2019-09-04 17:13:32 +0800

    change taskno type to string

--- processing/src/main/java/org/apache/carbondata/processing/merger/AbstractResultProcessor.java
+++ processing/src/main/java/org/apache/carbondata/processing/merger/AbstractResultProcessor.java
@@ -59,7 +59,7 @@ public abstract class AbstractResultProcessor {
       carbonDataFileAttributes = new CarbonDataFileAttributes(index, loadModel.getFactTimeStamp());
     } else {
       carbonDataFileAttributes =
-          new CarbonDataFileAttributes(Long.parseLong(loadModel.getTaskNo()),
+          new CarbonDataFileAttributes(loadModel.getTaskNo(),
               loadModel.getFactTimeStamp());
     }
     carbonFactDataHandlerModel.setCarbonDataFileAttributes(carbonDataFileAttributes);
